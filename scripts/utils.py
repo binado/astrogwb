44313577af655189ad8fc680faa5b37b698ffab7
binado <bernardopveronese@gmail.com>
2026-02-16 17:00:49 +0100

    refactor: use pydantic-settings to parse cli opts and config files

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,14 @@
 import subprocess
+from pathlib import Path
+
+
+def get_config_filepath(filepath: Path, suffix: str = ".toml") -> Path:
+    parent = filepath.parent
+    if parent != Path(__file__).parent:
+        raise ValueError(f"Config file must be in the same directory as {__file__}")
+    filename = filepath.stem
+    config_filepath = parent.parent / f"{filename}.config.toml"
+    return config_filepath.with_suffix(suffix)
 
 
 def get_git_revision() -> str | None:
